48c97f5ee9da83dc2491b3f228dc452d58590fa4
shmy137 <shaamirmansoor153@gmail.com>
2026-08-18 12:48:19 +0530

    Initial commit

---
+++ portfolio.js
@@ -0,0 +1,18 @@
+
+function sendMail(){
+let mes = {
+name : document.getElementById("name").value,
+subject : document.getElementById("subject").value,
+message : document.getElementById("message").value
+}
+    emailjs.send("service_2go8xve","template_aa31quv",mes)
+    .then(function(){
+  alert("Email send");
+
+    })
+
+    .catch(function(){
+        // console.log(error);
+        alert("error");
+    });     
+}
