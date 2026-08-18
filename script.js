45a25b0323c4ae5bdc3e5b8df9f19d086cbdd0cb
shmy137 <shaamirmansoor153@gmail.com>
2026-08-18 16:49:16 +0530

    .

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 function sendMail(){
 let mes = {
 name : document.getElementById("name").value,
+email : document.getElementById("email").value,
 subject : document.getElementById("subject").value,
 message : document.getElementById("message").value
 }
     emailjs.send("service_2go8xve","template_aa31quv",mes)
     .then(function(){
   alert("Email send");
-
     })
 
     .catch(function(){
